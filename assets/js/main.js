fafa597c1f6b0197a57af4d064f4be2a28c7d3fe
atixeret <atixeret@gmail.com>
2022-10-06 16:31:51 -0500

    Actualizacion 0610

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,6 @@
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
 
   /**
    * Easy event listener function
