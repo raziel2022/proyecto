2d99e3af57400d0b859e6a237163d09d05072efe
atixeret <atixeret@gmail.com>
2022-10-06 16:44:51 -0500

    Actualizacion 0610 v2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,17 @@
 (function() {
   "use strict";
 
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
 
   /**
    * Easy event listener function
