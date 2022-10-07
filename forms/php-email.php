e9c945714fa1a373ed4fca54f6f8e9bf9c6ce057
atixeret <atixeret@gmail.com>
2022-10-07 11:41:49 -0500

    Actualizacion correo

--- forms/php-email.php
+++ forms/php-email.php
@@ -1,23 +1,32 @@
 <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  
+
 <?php
-require 'class.phpmailer.php';
-require 'class.smtp.php';
+echo $_POST['message'];
 
 
-$enlace = $_POST['email'];     
-$encabezado= $_POST['subject']
-$cuerpo =  $_POST['message'];
-$destinatario = 'atixeret@gmail.com';
+require '../assets/vendor/php-email-form/class.phpmailer.php';
+require '../assets/vendor/php-email-form/class.smtp.php';
+
+$enlace = 'cojabmia@gmail.com';     
+$destinatario = $_POST['email']; 
+$asunto = $_POST['subject'];
+$encabezado =$_POST['subject'];
+$cuerpo =   '<html><body><p>Envio un correo '.$_POST[name] .' </p>
+
+<p>Mensaje que fue enviado es <strong>'.$_POST[message].'</strong></p>
+
+<p>Responder al correo <strong>' .$_POST[email].'</strong><br>
+
+Saludos</p>';
 
 
 mail($destinatario,$asunto,$cuerpo) ;
 
 $mail .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 $mail = new PHPMailer;
-$mail->setFrom('atixerte@gmail.com');
+$mail->setFrom($destinatario);
 $mail->addAddress($enlace);
 $mail->Subject = $encabezado;
 $mail->Body = $cuerpo;
@@ -28,19 +37,15 @@ $mail->SMTPSecure = 'ssl';
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
 $mail->Port = 465;
-$mail->Username = 'atixerte@gmail.com';
-$mail->Password = '!ToDaS%7SoNa!';
+$mail->Username = 'cojabmia@gmail.com';
+$mail->Password = 'lmyjbeefbdzpvhna';
 
 if(!$mail->send()) {
   echo 'Email no enviado.';
   echo 'Email error: ' . $mail->ErrorInfo;
 } else {
-  //echo 'Email enviado. '. $asunto;
-  header("Location: ../../index.html"); 
+  echo 'Email enviado. '. $asunto;
+  header("Location: ../index.html?signup=success"); 
   
 }
-  	
 ?>
-
- 
-	
